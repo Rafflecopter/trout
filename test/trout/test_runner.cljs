651c75028e5ae4f736e1a62a703582e4345884f9
Juan Patten <jr@juanpatten.com>
2015-12-07 16:47:52 -0700

    whoops... add settings test to test runner

--- test/trout/test_runner.cljs
+++ test/trout/test_runner.cljs
@@ -1,7 +1,9 @@
 (ns trout.test-runner
   (:require [doo.runner :refer-macros [doo-tests]]
+            [trout.settings-test]
             [trout.route-test]
             [trout.core-test]))
 
 (doo-tests 'trout.route-test
-           'trout.core-test)
+           'trout.core-test
+           'trout.settings-test)
