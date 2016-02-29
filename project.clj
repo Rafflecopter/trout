164ba4a5a2c94061f5415977b45c1fe639d5dce1
Juan Patten <jr@juanpatten.com>
2016-02-29 16:16:15 -0700

    new snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject trout "0.1.1"
+(defproject trout "0.1.2-SNAPSHOT"
   :description "A readable 'routes as data' routing library for clojurescript"
   :url "https://github.com/Rafflecopter/trout"
   :license {:name "Unlicense"
